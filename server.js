7e37cb8ed161abd913d88f929e38501ab4a7cbe6
johnnytortorici <johnny.tortorici@gmail.com>
2020-07-02 16:54:33 -0400

    Exercise 2

--- server.js
+++ server.js
@@ -26,6 +26,23 @@ express()
         }, randomTime);
   })
 
+  .get('/monkey-message', (req, res) => {
+    const messages = [
+      'Don’t monkey around with me.',
+      'If you pay peanuts, you get monkeys.',
+      'I fling 💩 at you!',
+      '🙊',
+      '🙈',
+      '🙉',
+    ];
+    const randomMsgIndex = Math.round(Math.random() * 5);
+    const message = { author: 'monkey', text: messages[randomMsgIndex] };
+    const randomTime = Math.floor(Math.random() * 3000);
+      setTimeout( () => {
+        res.status(200).json({ status: 200, message });
+      }, randomTime);
+  })
+
   // add new endpoints here ☝️
   // ---------------------------------
   // Nothing to modify below this line
